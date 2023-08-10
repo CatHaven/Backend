8eeb8e9706f5ea71ffa89ce6e08a1939b465cfb8
Nithin Muthukumar <kathrikai30201094@gmail.com>
2023-08-09 21:10:52 -0400

    endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 const mysql = require("mysql2");
+const jwt = require("jsonwebtoken");
 var router = express.Router();
 require("dotenv").config();
 
@@ -10,11 +11,11 @@ var connection = mysql.createConnection({
   database: process.env.DB,
   port: 3306,
 });
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
-router.get("/cats", async function(req, res, next) {
+router.get("/cats", async function (req, res, next) {
   connection.query("SELECT * FROM Cat", (err, results, fields) => {
     if (err) {
       console.error("Error executing", err);
@@ -23,7 +24,19 @@ router.get("/cats", async function(req, res, next) {
     res.send(results);
   });
 });
-router.get("/catweek", async function(req, res, next) {
+router.get("/cat/:name", async function (req, res, next) {
+  connection.query(
+    `SELECT * FROM Cat where CatName = '${req.params.name}'`,
+    (err, results, fields) => {
+      if (err) {
+        console.error("Error executing", err);
+        return;
+      }
+      res.send(results[0]);
+    }
+  );
+});
+router.get("/catweek", async function (req, res, next) {
   connection.query(
     "SELECT * FROM Cat where CatName = 'Phoenix'",
     (err, results, fields) => {
@@ -32,6 +45,58 @@ router.get("/catweek", async function(req, res, next) {
         return;
       }
       res.send(results);
+    }
+  );
+});
+function generateAuthToken(user) {
+  const payload = {
+    username: user.username,
+    email: user.email,
+    iat: Math.floor(Date.now() / 1000), // Issued at timestamp
+  };
+
+  return jwt.sign(payload, process.env.KEY);
+}
+
+router.get("/account", async function (req, res, next) {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401);
+  }
+  const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.KEY);
+  console.log(decoded);
+  res.json(decoded);
+});
+
+router.post("/register", async function (req, res, next) {
+  const data = req.body;
+  connection.query(
+    `INSERT INTO users(username, password, email) VALUES ("${data.username}", "${data.password}", "${data.email}")`,
+    (err, results, fields) => {
+      if (err) {
+        console.error("Error executing", err);
+        return;
+      }
+      res.send(results);
+    }
+  );
+});
+
+router.post("/login", async function (req, res, next) {
+  const data = req.body;
+  connection.query(
+    `SELECT * FROM users where username = "${data.username}"`,
+    (err, results, fields) => {
+      if (err) {
+        console.error("Error executing", err);
+        return;
+      }
+      const token = generateAuthToken(results[0]);
+      if (results[0].password === data.password) {
+        res.json({ token });
+      } else {
+        res.status(401);
+      }
     }
   );
 });
